bb215b92d5a1b3ea02113ba653f517fb193029b3
unknown <Trung1234>
2020-03-22 07:09:28 -0700

    practice code first

---
+++ SchoolManager/SchoolManager/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SchoolContext())
+            {
+                // Nhập tên 1 student mới cần tạo
+                Console.Write("Ten cua 1 Student moi: ");
+                var name = Console.ReadLine();
+
+                // Thêm 1 student mới  
+                var blog = new Student{ StudentName = name };
+                db.Students.Add(blog);
+                db.SaveChanges();
+
+                // Hiển thị các student trong database, sắp xếp theo Name
+                var query = from b in db.Students
+                            orderby b.StudentName
+                            select b;
+
+                Console.WriteLine("Tat ca student trong database :");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.StudentName);
+                }
+
+                Console.WriteLine("Go bat ky phim nao de thoat...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
